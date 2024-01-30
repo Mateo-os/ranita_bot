592cd12d637abe6f50df664700f39c257f2c4430
Mateo Ostorero <mateoostorero71@gmail.com>
2024-01-29 22:26:44 -0300

    test migrations

---
+++ migrations/20240130012619_noname.js
@@ -0,0 +1,87 @@
+const Sequelize = require("sequelize");
+
+/**
+ * Actions summary:
+ *
+ * createTable() => "Carta", deps: []
+ *
+ */
+
+const info = {
+  revision: 1,
+  name: "noname",
+  created: "2024-01-30T01:26:19.077Z",
+  comment: "",
+};
+
+const migrationCommands = (transaction) => [
+  {
+    fn: "createTable",
+    params: [
+      "Carta",
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          field: "id",
+          autoIncrement: true,
+          primaryKey: true,
+          allowNull: false,
+        },
+        nombre: { type: Sequelize.STRING, field: "nombre" },
+        rareza: { type: Sequelize.INTEGER, field: "rareza" },
+        number: { type: Sequelize.INTEGER, field: "number" },
+        createdAt: {
+          type: Sequelize.DATE,
+          field: "createdAt",
+          allowNull: false,
+        },
+        updatedAt: {
+          type: Sequelize.DATE,
+          field: "updatedAt",
+          allowNull: false,
+        },
+      },
+      { transaction },
+    ],
+  },
+];
+
+const rollbackCommands = (transaction) => [
+  {
+    fn: "dropTable",
+    params: ["Carta", { transaction }],
+  },
+];
+
+const pos = 0;
+const useTransaction = true;
+
+const execute = (queryInterface, sequelize, _commands) => {
+  let index = pos;
+  const run = (transaction) => {
+    const commands = _commands(transaction);
+    return new Promise((resolve, reject) => {
+      const next = () => {
+        if (index < commands.length) {
+          const command = commands[index];
+          console.log(`[#${index}] execute: ${command.fn}`);
+          index++;
+          queryInterface[command.fn](...command.params).then(next, reject);
+        } else resolve();
+      };
+      next();
+    });
+  };
+  if (useTransaction) return queryInterface.sequelize.transaction(run);
+  return run(null);
+};
+
+module.exports = {
+  pos,
+  useTransaction,
+  up: (queryInterface, sequelize) =>
+    execute(queryInterface, sequelize, migrationCommands),
+  down: (queryInterface, sequelize) =>
+    execute(queryInterface, sequelize, rollbackCommands),
+  info,
+};

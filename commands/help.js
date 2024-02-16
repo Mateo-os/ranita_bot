270e69254452e7549196b01b4c8b31819fad83a9
RayDeve <rayandeveloper@proton.me>
2024-02-16 17:56:49 +0100

    Arreglo de los prefijos

--- commands/help.js
+++ commands/help.js
@@ -1,15 +1,17 @@
 const { EmbedBuilder } = require('discord.js');
+const config = require('./config/config.js');
+const pre = config.prefix;
 
 async function help(message){
     const infoE = new EmbedBuilder()
         .setColor(0x31593B)
         .setTitle(`Información sobre el bot`)
         .addFields(
-            { name: '/roll ', value: `Cada tirada son 5 personajes. Se consiguen mas tiradas diariamente (1 cada 24 horas) y según <@273266775481778177> vaya liberando.`, inline: false },
-            { name: '/album', value: `Muestra todos los personajes que tienes.`, inline: false },
-            { name: '/giftrolls', value: "Ponés un número, mencionás a un usuario y le regalás las rolls indicadas.", inline: false },
-            { name: '/info', value: "Te da tu data", inline: false },
-            { name: '/checkcards', value: "Comprobás si tenés el personaje que escribís", inline: false },
+            { name: `${pre}roll `, value: `Cada tirada son 5 personajes. Se consiguen mas tiradas diariamente (1 cada 24 horas) y según <@273266775481778177> vaya liberando.`, inline: false },
+            { name: `${pre}album`, value: `Muestra todos los personajes que tienes.`, inline: false },
+            { name: `${pre}giftrolls`, value: "Ponés un número, mencionás a un usuario y le regalás las rolls indicadas.", inline: false },
+            { name: `${pre}info`, value: "Te da tu data", inline: false },
+            { name: `${pre}checkcards`, value: "Comprobás si tenés el personaje que escribís", inline: false },
             { name: 'Cuestiones tecnicas', value: "Contacte <@530487646766497792> o <@441325983363235841>", inline: false },
         ).setImage('https://www.manimalworld.net/medias/images/alytesmuletensis.jpg');
     message.channel.send({ embeds: [infoE] });

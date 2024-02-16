829cf53536b08ef41591780bfdebd83eb84ae7b2
mateo_ostorero <mateo.ostorero@splight-ae.com>
2024-02-16 18:31:46 -0300

    added empty case to album

--- commands/album.js
+++ commands/album.js
@@ -1,11 +1,14 @@
 const parseCartas = require("./parse.js");
 const findplayer = require("./findplayer.js");
 
-async function album(player, message){
-    let usuario = (message.mentions.members.first())? 
-        await findplayer(message.mentions.members.first().user.id,message.guild.id):player;
-    return [`Estas son las cartas totales de ${usuario.nombre.toUpperCase()}: \n`,
-        parseCartas(usuario.cartas,showRepeats=true)];
+async function album(player, message) {
+    let usuario = (message.mentions.members.first()) ?
+        await findplayer(message.mentions.members.first().user.id, message.guild.id) : player;
+    if (!usuario.cartas) {
+        return [`${usuario.nombre} no posee cartas`];
+    }
+    return [`Estas son las cartas totales de ${usuario.nombre}: \n`,
+    parseCartas(usuario.cartas, showRepeats = true)];
 }
 
 module.exports = album;

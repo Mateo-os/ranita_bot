1060497214a88c24ba6b44e6b51d358fccb6c0b8
Mateo Ostorero <mateoostorero71@gmail.com>
2024-02-16 12:51:42 -0300

    startup file

--- populate.sh
+++ populate.sh
@@ -2,7 +2,12 @@
 
 # Check if the POPULATED variable is not set
 if [ -z "${POPULATED}" ]; then
-    # Run the command to populate and set the POPULATED variable
+    # Run the Sequelize migrations
+    npx sequelize-cli db:migrate
+
+    # Run the script to populate data
     node populate.js
+
+    # Set the POPULATED variable to 1
     export POPULATED=1
 fi

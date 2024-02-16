825f13e2db3ea8eb719255c6bcdb8c1d32062afd
mateo_ostorero <mateo.ostorero@splight-ae.com>
2024-02-16 18:35:50 -0300

    lenght bug

--- commands/album.js
+++ commands/album.js
@@ -4,7 +4,7 @@ const findplayer = require("./findplayer.js");
 async function album(player, message) {
     let usuario = (message.mentions.members.first()) ?
         await findplayer(message.mentions.members.first().user.id, message.guild.id) : player;
-    if (!usuario.cartas) {
+    if (!usuario.cartas.length) {
         return [`${usuario.nombre} no posee cartas`];
     }
     return [`Estas son las cartas totales de ${usuario.nombre}: \n`,

b09073468e0c5718db3383bd53f2eb82bfaa8ab1
Mateo Ostorero <mateoostorero71@gmail.com>
2024-02-21 10:29:27 -0300

    styling

--- commands/repeats.js
+++ commands/repeats.js
@@ -5,9 +5,12 @@ async function repeats(player, message) {
     const usuario = (message.mentions.members.first()) ?
         await findplayer(message.mentions.members.first().user.id, message.guild.id) : player;
     const cartas = usuario.cartas.filter(c => c.Cromo.cantidad > 1);
-    if (!cartas.length) 
-        return [`${usuario.nombre} no posee duplicados`];
-    const header = (player.id_discord == usuario.id_discord) ? `Estas son tus cartas duplicadas`:
+    const selfcheck = (player.id_discord == usuario.id_discord); 
+    if (!cartas.length){
+        const response =  selfcheck ? "No posees duplicados": `${usuario.nombre} no posee duplicados`
+        return [response];
+    }
+    const header = selfcheck ? `Estas son tus cartas duplicadas`:
         `Estas son las cartas duplicadas de ${usuario.nombre}: \n`;
     return [header,
     parseCartas(cartas, showRepeats = true)];

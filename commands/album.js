c89378d93c5829ba66026bcfb68abc5179cb4224
Mateo Ostorero <mateoostorero71@gmail.com>
2024-02-27 11:15:42 -0300

    sorted album

--- commands/album.js
+++ commands/album.js
@@ -2,9 +2,15 @@ const {parseCartas} = require("../helpers");
 const findplayer = require("./findplayer.js");
 
 async function album(player, message) {
-    let usuario = (message.mentions.members.first()) ?
+    const usuario = (message.mentions.members.first()) ?
         await findplayer(message.mentions.members.first().user.id, message.guild.id) : player;
-    return [usuario, usuario.cartas]  
+    const album =  usuario.cartas.sort((c1,c2) => {
+        const comp = c1.serie.localeCompare(c2.serie);
+        if(comp) return comp;
+        else return c1.numero - c2.numero;
+    });
+    
+    return [usuario,album]  
 }
 
 module.exports = {album};

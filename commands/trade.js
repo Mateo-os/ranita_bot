af6144e1091f15c0112e7789597c43eb20ef519a
Mateo Ostorero <mateoostorero71@gmail.com>
2024-03-24 14:51:46 -0300

    fix rebase

--- commands/trade.js
+++ commands/trade.js
@@ -3,8 +3,24 @@ const { parseCartas } = require('../helpers');
 
 
 async function trade(player, message, args){
-
-    const text = parseCartas(player.cartas.slice(0,2),showRepeats = true)
+    const mentionedMember = message.mentions.members.first();
+    const result = [];
+    if (!mentionedMember){
+        result.push(`Debes mencionar a otro usuario`,[]);
+        return result;
+    }
+    const member = await findplayer(member_id, server_id);
+    const member_id = mentionedMember.user.id
+    const server_id = message.guild.id;
+    //Remome ids mentions using regular expressions
+    const name = (args.join(' ').replace(/<(@[0-9]+)>/g, '')).trim() || "";
+    if( name.length == 0){
+        result.push(`No diste ningun nombre`,[]);
+        return result;
+    }
+    
+    const cards = player.cartas.cartas.filter(c => new RegExp(cardName, 'i').test(c.nombre));
+    const text = parseCartas(cards,showRepeats = true)
  
     const embed = new EmbedBuilder().setDescription(text)
     await message.channel.send({embeds: [embed]});

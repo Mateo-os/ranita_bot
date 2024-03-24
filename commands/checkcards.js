4be135b04ff6889c286c582ce8669f5c1bc29d44
Mateo Ostorero <mateoostorero71@gmail.com>
2024-03-24 14:44:23 -0300

    FIX empty case checkcards

--- commands/checkcards.js
+++ commands/checkcards.js
@@ -17,13 +17,20 @@ async function checkcards(player, message, args) {
     }
     const selfcheck = player.id_discord == member.id_discord;
     if (!member) {
-        result.push('Ese usuario no tiene un perfil activo',[]);
+        result.push(
+            'Ese usuario no tiene un perfil activo',
+            []
+        );
+        return result;
     }
     const cartas = member.cartas.filter(c => new RegExp(name, 'i').test(c.nombre));
     if (cartas.length == 0) {
-        return [
-            `${selfcheck ? 'No tienes' : `<@${member.id_discord}> no tiene`} cartas con ese nombre o similares.`
-        ];
+        result.push(
+            `${selfcheck ? 'No tienes' : `<@${member.id_discord}> no tiene`} cartas con ese nombre o similares.`,
+            []
+        );
+        return result;
+
     }    
     result.push(`Estas son ${selfcheck ? `tus cartas` : `las cartas de <@${member.id_discord}>`} similares a \"${name}\"`);
     result.push(parseCartas(cartas, showRepeats = true));

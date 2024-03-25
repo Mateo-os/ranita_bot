ba77bc8b299440ffe41c5f6c4c4a485e5c570592
Mateo Ostorero <mateoostorero71@gmail.com>
2024-03-24 22:45:48 -0300

    started work on trade

--- helpers/index.js
+++ helpers/index.js
@@ -1,6 +1,6 @@
-const {newPanel,sendCardEmbed, sendCardListEmbed, sendCardDropDownEmbed} = require('./embed.js');
-const {parseCartas} = require('./parse.js');
-const {rarities,EPHIMERAL_ROLL_LIMIT,CARDS_PER_ROLL} = require('./constants.js');
+const { newPanel, sendCardEmbed, sendCardListEmbed, sendCardDropDownEmbed } = require('./embed.js');
+const { parseCartas } = require('./parse.js');
+const { rarities, EPHIMERAL_ROLL_LIMIT, CARDS_PER_ROLL } = require('./constants.js');
 
 module.exports = {
     newPanel,

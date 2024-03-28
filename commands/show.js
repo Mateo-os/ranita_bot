2e0a9b32a7f4da24d265b598411952b44e0bb612
mateo_ostorero <mateo.ostorero@splight-ae.com>
2024-03-28 10:37:16 -0300

    reverted show changes

--- commands/show.js
+++ commands/show.js
@@ -1,30 +1,30 @@
 async function show(responses, message, ephemeral = false) {
-    const replies = [];
-    for(let i in responses){
+    const messagelist = [];
+    for (let i in responses) {
         r = responses[i]
-        if(r.length < 2000){
+        if (r.length < 2000) {
             message.channel.send(r);
             continue;
         }
-        const wrap = (s) => {return '\`\`\`' + s + '\`\`\`'};
+        const wrap = (s) => { return '\`\`\`' + s + '\`\`\`' };
         const mono = r.startsWith('\`');
         let temp = r.replace(/\`\`\`/g, "");
         let index = 0;
 
-        while(index < temp.length){
+        while (index < temp.length) {
             const end = index + 1001;
             const tail = temp.slice(end);
-            let buffer = temp.slice(index,end);
-            buffer += tail.slice(0,tail.indexOf('\n'));
+            let buffer = temp.slice(index, end);
+            buffer += tail.slice(0, tail.indexOf('\n'));
             index += buffer.length;
-            if(mono){
+            if (mono) {
                 buffer = wrap(buffer);
             }
-            replies.push(await message.reply({content:buffer,ephemeral:ephemeral}));
+            messagelist.push(await message.channel.send(buffer));
         }
     }
     responses.length = 0;
-    return replies
+    return messagelist;
 }
 
-module.exports = {show};
+module.exports = { show };

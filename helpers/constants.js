4ee28e61131afff9c52afb13dafe881fdba99073
Mateo-os <mateoostorero71@gmail.com>
2024-04-02 11:47:27 -0300

    constants

--- helpers/constants.js
+++ helpers/constants.js
@@ -2,13 +2,22 @@ const EPHIMERAL_ROLL_LIMIT = 15;
 
 const CARDS_PER_ROLL = 3;
 
-const rarities ={
-    1:'COMÚN',
-    2:'POCO COMÚN',
-    3:'RARA',
-    4:'ÉPICA',
-    5:'LEGENDARIA',
-    6:'IMPOSIBLE',
+const rarities = {
+    1: 'COMÚN',
+    2: 'POCO COMÚN',
+    3: 'RARA',
+    4: 'ÉPICA',
+    5: 'LEGENDARIA',
+    6: 'IMPOSIBLE',
 }
 
-module.exports = {rarities,EPHIMERAL_ROLL_LIMIT,CARDS_PER_ROLL}
+const recycle_points = {
+    1: 2,
+    2: 2,
+    3: 5,
+    4: 20,
+    5: 200
+}
+
+
+module.exports = { rarities, recycle_points, EPHIMERAL_ROLL_LIMIT, CARDS_PER_ROLL }

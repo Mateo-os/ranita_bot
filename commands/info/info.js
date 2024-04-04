d72a7d215147748af6a29a5d4300fb51a640d81e
Mateo Ostorero <mateoostorero71@gmail.com>
2024-04-04 12:39:00 -0300

    Changed how info display information

--- commands/info/info.js
+++ commands/info/info.js
@@ -1,20 +1,46 @@
 const { models } = require('../../database');
-const { fn, col } = require('sequelize');
+const { fn, col, Op} = require('sequelize');
 
 async function info(player) {
     const result = [];
-    const total_cards = await models.Carta.count();
-    const cardQuery = await models.Carta.findAll({
+    const all_cards = await models.Carta.findAll({ 
+        where: { 
+            rareza: {
+                [Op.lte]: 5,
+            
+            }
+        },
+    });
+    const total_cards = all_cards.length;
+    const card_agreggation = await models.Carta.findAll({
         attributes: ['rareza', [fn('COUNT', col('*')), 'count']],
+        where: {
+            rareza: {
+                [Op.lte]: 5,
+            }
+        },
         group: ['rareza'],
     });
 
-    cardQuery.sort((a, b) => a.rareza - b.rareza);
-    const inventory = player.cartas.reduce((counts, carta) => {
+    card_agreggation.sort((a, b) => a.rareza - b.rareza);
+    const cards = player.cartas.filter(c => c.rareza <= 5);
+    const r6 = player.cartas.filter(c => c.rareza >= 6);
+    const r6_sisters = r6.map(c => all_cards.find(cc => (cc.numero == c.numero) && (cc.serie == c.serie) && (cc.rareza < 6)));
+    let inventory = cards.reduce((counts, carta) => {
         counts[carta.rareza] = (counts[carta.rareza] || 0) + 1;
         return counts;
     }, {});
+    
+    let card_count = cards.length;
     const repetidas = player.cartas.filter(c => c.Cromo.cantidad > 1);
+    r6_sisters.forEach(c => {
+        const card = cards.find(card => card.id == c.id);
+        if(!card)
+        {
+            inventory[c.rareza] = (inventory[c.rareza] || 0) + 1;
+            card_count += 1;
+        }
+    });
     const repeat_amount = repetidas.reduce((acc, carta) => acc + (carta.Cromo.cantidad - 1), 0);
 
     let info = "\`\`\`"
@@ -22,9 +48,9 @@ async function info(player) {
         `Rolls: ${player.rolls}
 Rolls efímeros: ${player.freerolls}
 Cartas repetidas: ${repeat_amount}
-Colección: ${player.cartas.length}/${total_cards}
+Colección: ${card_count}/${total_cards}
 Rarezas:\n`;
-    cardQuery.forEach(c => {
+    card_agreggation.forEach(c => {
         info += `Rareza ${c.rareza}: ${inventory[c.rareza] || 0}/${c.get('count')}\n`;
     })
     info += '\`\`\`';

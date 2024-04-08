7ffa3cd1e4b6a756733140df478d0c8d6c09e48e
Mateo Ostorero <mateoostorero71@gmail.com>
2024-04-08 14:47:24 -0300

    Better populate script

--- populate.js
+++ populate.js
@@ -34,7 +34,29 @@ seriesArray.forEach(series => {
             if (created) {
                 console.log(`Added ${elementName} to ${seriesName}`);
             } else {
-                console.log(`Skipped duplicate entry for ${elementName} in ${seriesName}`);
+                let change = false;
+                let output = '';
+                let changes = '';
+                if(carta.numero != elementNumber) {
+                    changes += `Number changed from ${carta.numero} to ${elementNumber}. ` 
+                    carta.numero = elementNumber;
+                    change = true;
+                }else if(carta.rareza != elementRarity){
+                    changes += `Rarity changed from ${carta.rareza} to ${elementRarity}. `;
+                    carta.rareza = elementRarity;
+                    change = true;
+                }else if(carta.URLimagen != elementURL){
+                    changes += `URL changed from ${carta.URLimagen} to ${elementURL}. `;
+                    carta.URLimagen = elementURL;
+                    change = true;
+                }
+                if(change){
+                    output = `Detected changes for ${elementName} in ${seriesName}: ${changes}`;
+                    carta.save();
+                } else{
+                    output = `Skipped duplicate entry for ${elementName} in ${seriesName}.`;
+                }
+                console.log(output);
             }
         }).catch(error => {
             console.error(`Error adding ${elementName} to ${seriesName}: ${error.message}`);

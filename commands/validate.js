ff31089be54528b22b2c70005bcf0a2b1a62fff9
Matnamalo <mateoostorero71@gmail.com>
2025-08-26 20:50:12 -0300

    Rebased with current version

--- commands/validate.js
+++ commands/validate.js
@@ -3,9 +3,9 @@ const { config } = require('../config/config');
 const globalServerConfig = config.serverConfig || {};
 
 function validateCommand(server_id, channel, command) {
-    if (!(server_id in globalServerConfig))
+    if (!(server_id in globalServerConfig)){
         return [true, []];  // Allow by default if no specific config exists for the server
-
+    }
     let enable = true;
     const response = [];
     const serverConfig = globalServerConfig[server_id];
@@ -19,13 +19,19 @@ function validateCommand(server_id, channel, command) {
 
     // Fetch the channel config. If it not exist, use the default 
     const channelConfig = channel in serverConfig.CHANNELS ? serverConfig.CHANNELS[channel] : serverConfig.DEFAULT;
+    // First check for explicit allowance
+    if (channelConfig.allow.includes(command)){
+        enable = true
+        return [enable,response]
+    }
+    // Check that the command isn't denied in any way
     // Check for general exclusion, 
     // Then check for explicit exclusion, otherwise
-    // check for absence of general or explicit inclusion
+    // Otherwhise check for absence of general inclusion
     // (channelConfig.allow.includes('*') || channelConfig.allow.length === 0) means that every command is allowed. 
     if( channelConfig.deny.includes('*')     || 
         channelConfig.deny.includes(command) || 
-        (!(channelConfig.allow.includes('*') || channelConfig.allow.length === 0) && !channelConfig.allow.includes(command))
+        (!(channelConfig.allow.includes('*') || channelConfig.allow.length === 0))
     ) {
         enable = false;
         response.push(`El comando ${prefix}${command} no existe o está deshabilitado en este canal.`);

c9623504eabf937662ecccd401828a75d0603c43
Matnamalo <mateoostorero71@gmail.com>
2025-08-26 22:09:10 -0300

    refined inclusion/exclusion rules

--- commands/validate.js
+++ commands/validate.js
@@ -3,41 +3,40 @@ const { config } = require('../config/config');
 const globalServerConfig = config.serverConfig || {};
 
 function validateCommand(server_id, channel, command) {
+    const emptyResponse = []
     if (!(server_id in globalServerConfig)){
-        return [true, []];  // Allow by default if no specific config exists for the server
+        // Allow by default if no specific config exists for the server
+        return [true, emptyResponse];  
     }
-    let enable = true;
-    const response = [];
     const serverConfig = globalServerConfig[server_id];
     const prefix = serverConfig.PREFIX
     // Check if the command is banned globally
     if (serverConfig.BANNED_COMMANDS.includes(command)) {
-        enable = false;
-        response.push(`El comando ${prefix}${command} está prohibido en el servidor.`);
-        return [enable, response];
+        return [false, [`El comando ${prefix}${command} está prohibido en el servidor.`]];
     }
 
     // Fetch the channel config. If it not exist, use the default 
     const channelConfig = channel in serverConfig.CHANNELS ? serverConfig.CHANNELS[channel] : serverConfig.DEFAULT;
+    const forbidenCommandResponse = [`El comando ${prefix}${command} no existe o está deshabilitado en este canal.`] 
+    
     // First check for explicit allowance
     if (channelConfig.allow.includes(command)){
-        enable = true
-        return [enable,response]
-    }
-    // Check that the command isn't denied in any way
-    // Check for general exclusion, 
-    // Then check for explicit exclusion, otherwise
-    // Otherwhise check for absence of general inclusion
-    // (channelConfig.allow.includes('*') || channelConfig.allow.length === 0) means that every command is allowed. 
-    if( channelConfig.deny.includes('*')     || 
-        channelConfig.deny.includes(command) || 
-        (!(channelConfig.allow.includes('*') || channelConfig.allow.length === 0))
-    ) {
-        enable = false;
-        response.push(`El comando ${prefix}${command} no existe o está deshabilitado en este canal.`);
+        return [true,emptyResponse]
     }
 
-    return [enable, response];
+    // If no explicit allow, then check for explicit or global deny
+    if (channelConfig.deny.includes('*') || channelConfig.deny.includes(command)) {
+        return [false, forbidenCommandResponse];
+    }
+
+    // Check general allow either with wildcard or empty list 
+    if (channelConfig.allow.includes('*') || channelConfig.allow.length === 0) {
+        return [true, emptyResponse];
+    }
+
+    // Deny if no rules are matched.
+    // Pre-validation garantuees "*"" can't exists in both deny and allow.
+    return [false, forbidenCommandResponse]    
 }
 
 module.exports = {
